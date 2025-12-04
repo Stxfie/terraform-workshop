feaff376a398be2ad5da5a44c630b41da7979f9b
Stefan Weijers <58026162+Stxfie@users.noreply.github.com>
2025-12-04 14:56:21 +0100

    just keep rolling rolling rolling rolling

---
+++ aap_job.tf
@@ -0,0 +1,29 @@
+data "aap_job_template" "deploy_webserver" {
+  name              = "DEPLOY_WEBSERVER_SRW"
+  organization_name = var.organization_name
+}
+
+data "aap_job_template" "deploy_website" {
+  name              = "DEPLOY_WEBSITE_SRW"
+  organization_name = var.organization_name
+}
+
+resource "aap_job" "deploy_webserver_job" {
+  job_template_id = aap_job_template.deploy_webserver.id
+  inventory_id    = aap_inventory.srw_inventory.id
+
+  depends_on = [
+    aap_host.test_host,
+    aap_group.test_group
+  ]
+}
+
+resource "aap_job" "deploy_website_job" {
+  job_template_id = aap_job_template.deploy_website.id
+  inventory_id    = aap_inventory.srw_inventory.id
+
+  depends_on = [
+    aap_host.test_host,
+    aap_group.test_group
+  ]
+}
